9c2527d406ac403df147d8cae09e32ef62b64ed0
Mark Wiltberger <mark.wiltberger@gmail.com>
2023-08-18 17:03:06 -0400

    Changed baseURL to point to Docker image

--- src/helper.js
+++ src/helper.js
@@ -1,5 +1,7 @@
 // defining baseURL constant to be declared in multiple components
 
 export const server = {
-  baseURL: 'http://localhost:3080'
+  baseURL: 'http://localhost'
 }
+
+  // DEV baseURL:   baseURL: 'http://localhost:3000'
