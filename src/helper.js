3fc154398e29b6e38e05f56f9c3fd6ace6e7cf6f
Mark Wiltberger <mark.wiltberger@gmail.com>
2023-08-22 15:33:31 -0400

    Changed to baseURL for Google Cloud hosted backend

--- src/helper.js
+++ src/helper.js
@@ -1,7 +1,9 @@
 // defining baseURL constant to be declared in multiple components
 
 export const server = {
-  baseURL: 'http://localhost'
+  baseURL: 'https://customer-list-app-backend-template-1-iftelzoeoa-uc.a.run.app/customer'
 }
 
   // DEV baseURL:   baseURL: 'http://localhost:3000'
+  // Docker in DEV baseURL:      baseURL: 'http://localhost'
+  // Google Cloud Run baseURL:     baseURL: 'https://customer-list-app-backend-template-1-iftelzoeoa-uc.a.run.app'
